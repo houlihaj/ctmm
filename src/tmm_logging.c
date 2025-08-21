d3c4fbe63369a1dda0b1274465da8f93b550e1cb
houlihaj <houlihaj@uci.edu>
2025-08-21 19:02:41 -0400

    tmm_logging.c created in preparation to use log.c library.

---
+++ src/tmm_logging.c
@@ -0,0 +1,5 @@
+//
+// Created by johnh on 8/21/2025.
+//
+
+#include "tmm_logging.h"
---
+++ src/tmm_logging.h
@@ -0,0 +1,8 @@
+//
+// Created by johnh on 8/21/2025.
+//
+
+#ifndef CTMM_TMM_LOGGING_H
+#define CTMM_TMM_LOGGING_H
+
+#endif //CTMM_TMM_LOGGING_H
